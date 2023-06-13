44da6b750aa944d33d7f6c361aab9bf4f634e991
BartekMTB <117111323+BartekMTB@users.noreply.github.com>
2023-06-13 10:28:30 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,49 +1,33 @@
 import React, { Component } from 'react';
 import css from './App.module.css';
-
+import { nanoid } from 'nanoid';
 export class App extends Component {
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    total: 0,
-  };
-
-  handleOnClickGoodGrade = () => {
-    this.setState({ good: this.state.good + 1, total: this.state.total + 1 });
-  };
-  handleOnClickNeutralGrade = () => {
-    this.setState({
-      neutral: this.state.neutral + 1,
-      total: this.state.total + 1,
-    });
-  };
-  handleOnClickBadGrade = () => {
-    this.setState({ bad: this.state.bad + 1, total: this.state.total + 1 });
+    contacts: [],
+    name: '',
   };
 
   render() {
     return (
       <div className={css.main_div}>
-        <h2>Please leave feedback</h2>
-        <div>
-          <button onClick={this.handleOnClickGoodGrade}>Good</button>
-          <button onClick={this.handleOnClickNeutralGrade}>Neutral</button>
-          <button onClick={this.handleOnClickBadGrade}>Bad</button>
-        </div>
-        <h2>Statistics</h2>
-        <div>
-          <p>Good: {this.state.good}</p>
-          <p>Neutral: {this.state.neutral}</p>
-          <p>Bad: {this.state.bad}</p>
-          <p>Total: {this.state.total}</p>
-          <p>
-            Positive feedback:
-            {this.state.good > 0 &&
-              ((this.state.good / this.state.total) * 100).toFixed(0)}
-            %
-          </p>
-        </div>
+        <h2>Phonebook</h2>
+        <form>
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <button>Add contacts</button>
+        </form>
+        <h2>Contacts</h2>
+        <ul>
+          <li>ddsda aasd</li>
+          <li>asdasd sadasd</li>
+        </ul>
+        <h3>{nanoid()}</h3>
       </div>
     );
   }
