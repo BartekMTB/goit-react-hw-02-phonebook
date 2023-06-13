946f8170f5ad6e981a8321409549c994dbab345b
BartekMTB <117111323+BartekMTB@users.noreply.github.com>
2023-06-13 18:52:14 +0200

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,25 @@ import css from './App.module.css';
 import { nanoid } from 'nanoid';
 export class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      { key: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { key: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { key: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { key: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
     name: '',
     number: '',
   };
 
-  /*  handleChange = evt => {
-    const { name, value } = evt.target;
-    this.setState({ [name]: value });
-  }; */
+  handleChange = evt => {
+    this.setState({ filter: evt.target.value });
+  };
+  filterItems = () => {
+    return this.state.contacts.filter(el =>
+      el.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    );
+  };
 
   handleSubmit = evt => {
     evt.preventDefault();
@@ -26,8 +35,6 @@ export class App extends Component {
         { key: nanoid(), name: name, number: number },
       ],
     });
-
-    //console.log(this.state.contacts);
     form.reset();
   };
 
@@ -36,7 +43,7 @@ export class App extends Component {
       <div className={css.main_div}>
         <h2>Phonebook</h2>
         <form onSubmit={this.handleSubmit}>
-          <p>Name</p>
+          <label>Name</label>
           <input
             type="text"
             name="name"
@@ -44,6 +51,7 @@ export class App extends Component {
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
+          <label>Phone</label>
           <input
             type="tel"
             name="number"
@@ -54,8 +62,16 @@ export class App extends Component {
           <button>Add contacts</button>
         </form>
         <h2>Contacts</h2>
+        <input
+          type="text"
+          name="filter"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          // value={filter}
+          onChange={this.handleChange}
+        />
         <ul>
-          {this.state.contacts.map(el => (
+          {this.filterItems().map(el => (
             <li key={el.key}>
               {el.name} {el.number}
             </li>
